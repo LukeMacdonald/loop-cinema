d3a27324978bc3f76ba7a38a211501e55309414e
LukeMacdonald <lukemacdonald21@gmail.com>
2023-09-19 21:44:04 +1000

    added movie model

---
+++ loopcinema-api/database/models/movie.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require("sequelize");
+
+sequelize.define("movie", {
+    movie_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    title:{
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    release_date:{
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    poster:{
+        type: DataTypes.BLOB,
+        allowNull: false
+    },
+    duration:{
+        type:DataTypes.INTEGER,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    }
+  },
+  {
+    // Don't add the timestamp attributes (updatedAt, createdAt).
+    timestamps: false
+  });
