083c3980e698c513a62e8582d118bb024abf434e
LukeMacdonald <lukemacdonald21@gmail.com>
2023-09-22 22:35:42 +1000

    added hashing password

--- loopcinema-api/controllers/user-controller.js
+++ loopcinema-api/controllers/user-controller.js
@@ -1,20 +1,36 @@
-db = require('../database')
+const db = require('../database')
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
-exports.insert = async (req,res) => {
 
-    const user = await db.user.create({
-        username:req.body.username,
-        password:req.body.password,
-        email:req.body.email,
-        name: req.body.name
-    });
+exports.insert = async (req, res) => {
+    const password = req.body.password;
+  
+    try {
+      const hash = await bcrypt.hash(password, saltRounds); 
+      console.log('Hash ', hash); 
+  
+      const user = await db.user.create({
+        username: req.body.username,
+        password: hash,
+        email: req.body.email,
+        name: req.body.name,
+      });
+  
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'An error occurred while creating the user.' });
+    }
+  };
 
-    res.json(user);
+exports.update = async (req,res) => {
+
 }
 
-exports.update = async (req,res) => {}
+exports.delete = async (req,res) => {
 
-exports.delete = async (req,res) => {}
+}
 
 exports.select = async (req,res) => {
     const id = req.params.id
