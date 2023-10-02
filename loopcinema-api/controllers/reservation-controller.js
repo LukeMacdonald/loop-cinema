4918eaa975a0d1a51b64c667942f208c6fd3dda0
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2023-10-02 11:09:24 +1100

    improved reservation ui

--- loopcinema-api/controllers/reservation-controller.js
+++ loopcinema-api/controllers/reservation-controller.js
@@ -3,18 +3,29 @@ const { validationResult } = require('express-validator');
 
 exports.createReservation = async (req,res) => {
     try{
-        const reservation = await db.reservation.create({
-            session_id: req.body.session_id,
-            username: req.body.username,
-            total_seats: req.body.total_seats
-        });
+        const session_id = req.body.session_id;
+        const total_seats_requested = req.body.total_seats;
+        const session = await db.session.findByPk(session_id);
+        if (!session) {
+            return res.status(404).json({ error: 'Session not found.' });
+        }
+        // Check if there are enough available seats
+        if (total_seats_requested <= session.available_seats) {
+            const reservation = await db.reservation.create({
+                session_id: session_id,
+                username: req.body.username,
+                total_seats: total_seats_requested
+            });
 
-        const session = await db.session.findByPk(req.body.session_id);
-        
-        session.available_seats -= req.body.total_seats
-        
-        await session.save();
-        res.status(201).json(reservation);
+            // Update available seats in the session
+            session.available_seats -= total_seats_requested;
+            await session.save();
+
+            res.status(201).json(reservation);
+        } else {
+            // Not enough available seats
+            res.status(400).json({ error: 'Not enough available seats.' });
+        }
     } catch (err) {
         console.error(err.message);
         res.status(500).json({ error: 'An error occurred while creating the review.' })
--- loopcinema-portal/src/components/MovieSessions.js
+++ loopcinema-portal/src/components/MovieSessions.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Button } from 'react-bootstrap';
 import { formatTime, formatDate } from '../utils/dates';
 import { useNavigate } from 'react-router-dom';
@@ -16,12 +16,20 @@ function groupSessionsByDate(sessions) {
 }
 
 function MovieSessions(props) {
-    const movie = props.movie;
-    const sessions = props.sessions;
-    const username = localStorage.getItem("user")
+    const { movie, sessions } = props;
+    const username = localStorage.getItem("user");
     const groupedSessions = groupSessionsByDate(sessions);
     const [selectedDate, setSelectedDate] = useState(null);
+    const containerRef = useRef(null);
     const navigate = useNavigate();
+
+    const scrollLeft = () => {
+        containerRef.current.scrollLeft -= 200; // Adjust the scroll distance as needed
+    };
+
+    const scrollRight = () => {
+        containerRef.current.scrollLeft += 200; // Adjust the scroll distance as needed
+    };
 
     const handleReservation = (sessionID) => {
         if (!username || username.trim() === "") {
@@ -34,35 +42,44 @@ function MovieSessions(props) {
     };
 
     const handleDateSelection = (date) => {
-        if (selectedDate === date) {
-            // If the selected date is clicked again, hide the sessions
-            setSelectedDate(null);
-        } else {
-            setSelectedDate(date);
-        }
+        setSelectedDate(date === selectedDate ? null : date);
     };
 
     return (
         <div>
             <h2>Sessions</h2>
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-                {Object.keys(groupedSessions).map((date, index) => (
-                    <Button key={index} onClick={() => handleDateSelection(date)} style={{ margin: '2rem 0.5rem 0 ' }}>
-                        {date}
-                    </Button>
-                ))}
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <button className='btn btn-secondary' onClick={scrollLeft} style={{marginRight:'1rem'}}>&lt;</button>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        overflowX: 'auto',
+                        padding: '1rem',
+                        position: 'relative',
+                        flex: '1',
+                    }}
+                    ref={containerRef}
+                >
+                    {Object.keys(groupedSessions).map((date, index) => (
+                        <div className='col-lg-2 col-md-3 col-5' key={index}>
+                            <Button onClick={() => handleDateSelection(date)} style={{ margin: '0 0.5rem', width: '90%' }}>
+                                {date}
+                            </Button>
+                        </div>
+                    ))}
+                </div>
+                <button className='btn btn-secondary' onClick={scrollRight} style={{marginLeft:'1rem'}}>&gt;</button>
             </div>
             <hr />
-            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginBottom: '4rem' }}>
                 {selectedDate &&
                     groupedSessions[selectedDate].map((session, sessionIndex) => (
-                        <Button
-                            key={sessionIndex}
-                            style={{ margin: '2rem 0.5rem 0 ' }}
-                            onClick={() => handleReservation(session.session_id)} // Pass a function reference here
-                        >
-                            {formatTime(session.session_time)}
-                        </Button>
+                        <div className='col-md-4 col-6' key={sessionIndex}>
+                            <Button style={{ margin: '2rem 0.5rem 0 ', width: '80%' }} onClick={() => handleReservation(session.session_id)}>
+                                {formatTime(session.session_time)}
+                            </Button>
+                        </div>
                     ))}
             </div>
         </div>
@@ -70,3 +87,4 @@ function MovieSessions(props) {
 }
 
 export default MovieSessions;
+
